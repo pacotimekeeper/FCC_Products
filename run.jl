e7730fd34ab3c3950964d5c505f6393e11a13a5a
pacotimekeeper <kaiipho@gmail.com>
2026-06-22 20:54:06 +0800

    updates

--- run.jl
+++ run.jl
@@ -1,28 +1,56 @@
-
 using Bonito
 
-# Create a reactive counter app
-app = App() do session
-    return DOM.div(
-	    DOM.h1("Welcome to FCC Product"),
-        DOM.h2("Site in construction")
-    )
+# Define a function to create the reactive counter app
+function create_app()
+    return App(title="FCC Products";indicator=ConnectionIndicator()) do session
+        DOM.div(
+            DOM.h1("FCC Products"),
+            DOM.h2("Distributed Brands"),
+            DOM.br(),
+            DOM.div(
+                style = Styles(
+                    "display" => "flex", 
+                    "flex-direction" => "row", # /* Places children side-by-side (default) */
+                    "justify-content" => "space-around", #/* Centers items horizontally */
+                    "gap" => "10px" #/* Adds spacing between items */
+                    ),
+                DOM.a(href = "https://www.baxter.com/",
+                    DOM.img(src="https://cdn.worldvectorlogo.com/logos/baxter.svg"; width="150px")
+                ),
+                DOM.a(href = "https://www.unitedorthopedic.com/",
+                    DOM.img(src="https://www.unitedorthopedic.com/wp-content/uploads/2025/01/United-logo-new.svg"; width="150px")
+                ),
+                DOM.a(href = "https://www.smith-nephew.com/en",
+                    DOM.img(src="https://mc-4895e938-de31-484c-aa80-830364-cdn-endpoint.azureedge.net/-/media/project/smithandnephew/sitesettings/logo.svg?rev=23cce43476814f859aaff33ced056dc9"; width="150px")
+                )
+            )
+        )
+    end
 end
 
-# Or serve it on a server
-try
-    if Sys.islinux()
-        server = Server(app, "0.0.0.0", 8080, proxy_url="http://202.175.118.43:6023")
-    elseif Sys.isapple()
-        server = Server(app, "0.0.0.0", 8080)
-    else
-        error("Unknow host")
+# Define a function to serve the app on a server
+function run_server(app)
+    try
+        if Sys.islinux()
+            server = Server(app, "0.0.0.0", 8080, proxy_url="http://202.175.118.43:6023")
+        elseif Sys.isapple()
+            server = Server(app, "0.0.0.0", 8080)
+        else
+            error("Unknown host")
+        end
+        
+        println("Server is running")
+        wait(server)
+    catch err
+        println(err)
+    finally
+        close(server)
+        println("Server has stopped")
     end
-    println("Server is running")
-    wait(server)
-catch err
-    println(err)
-finally
-    close(server)
-    println("Server is running")
+end
+
+# Main function to orchestrate the app creation and serving
+function (@main)(args)
+    app = create_app()
+    run_server(app)
 end
