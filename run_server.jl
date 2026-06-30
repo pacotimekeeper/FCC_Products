6fce74fa4c62e1494284705777fe056d2947b40c
pacotimekeeper <kaiipho@gmail.com>
2026-06-30 20:36:39 +0800

    add few routes

---
+++ run_server.jl
@@ -0,0 +1,17 @@
+using GenieFramework
+
+# using Genie, Genie.Renderer, Genie.Renderer.Html, Genie.Renderer.Json
+
+route("/hello.html") do
+  html("Hello World")
+end
+
+route("/hello.json") do
+  json("Hello World")
+end
+
+route("/hello.txt") do
+   respond("Hello World", :text)
+end
+
+up(8080, async = true)
--- table.md
+++
@@ -1,6 +0,0 @@
-
-| Feature        | Status        | Priority |
-| -------------- |:-------------:| --------:|
-| 3D Rendering   | ✓ Implemented | High     |
-| Interactivity  | ✓ Implemented | High     |
-| Styling        | ✓ Implemented | Medium   |
