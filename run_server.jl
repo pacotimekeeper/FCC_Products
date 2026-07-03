c4e06e8918962d5ef352d615a707d1461437ecc1
pacotimekeeper <kaiipho@gmail.com>
2026-07-03 21:46:35 +0800

    change async

--- run_server.jl
+++ run_server.jl
@@ -4,7 +4,7 @@ Pkg.activate(".")
 using GenieFramework
 Genie.loadapp()
 
-up(8080, "0.0.0.0", async = true)
+up(8080, "0.0.0.0", async = false)
 # using Genie, Genie.Renderer, Genie.Renderer.Html, Genie.Renderer.Json
 
 
